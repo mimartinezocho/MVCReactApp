15ce2c6726a380fcbf06ef7f0a7ceaf9b81b714a
Miguel <mimartinezocho@gmail.com>
2023-11-25 08:20:23 -0600

    Agregar archivos de proyecto.

---
+++ MVCReactApp/ClientApp/src/App.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>Hola Soy Jorge. Este es mi primer proyecto con REACT y .NET CORE</h1>
+        </div>
+    )
+}
+
+export default App
